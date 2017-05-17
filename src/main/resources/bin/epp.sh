d3f086e522138bb2e0bd6f4f9391e33598283a3f
dell <dell@DESKTOP-4UOAA90>
2017-05-17 13:42:12 +0300

    echo

--- src/main/resources/bin/epp.sh
+++ src/main/resources/bin/epp.sh
@@ -6,6 +6,7 @@ pwd
 for i in $@
 do
 	echo projecting out $i from the $PROTOCOL protocol in $SCRIBBLE into an fsm
+	echo Dimitar
 	echo sh $SCRIBBLEDIR/bin/scribblec.sh $SCRIBBLE  -fsm $PROTOCOL $i >$SCRIBBLEDIR/bin/generated/$i.fsm
 	sh $SCRIBBLEDIR/bin/scribblec.sh $SCRIBBLE  -fsm $PROTOCOL $i >$SCRIBBLEDIR/bin/generated/$i.fsm
 	echo transforming the fsm for $i into an easyFSM config file
