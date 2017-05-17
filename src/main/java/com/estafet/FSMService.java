0421a4aa6e025ac1aa17bcc7b9af023a3089f46f
dell <dell@DESKTOP-4UOAA90>
2017-05-17 10:59:09 +0300

    Fixed location.

--- src/main/java/com/estafet/FSMService.java
+++ src/main/java/com/estafet/FSMService.java
@@ -26,7 +26,7 @@ public class FSMService {
 	private FSM f;
 	private String myrole = "generic";
 	private final static String SCRIBBLEDIR = "/opt/app-root/src/src/main/resources"; 
-	private String location = SCRIBBLEDIR + "/src/main/resources";
+	private String location = SCRIBBLEDIR;// + "/src/main/resources";
 	static String urlString = "/fsmserver/api";
 	static String payload = urlString + " response.";
 
