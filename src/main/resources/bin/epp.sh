187c7189318c5baaa51a477cc53da57a2a795fe3
dell <dell@DESKTOP-4UOAA90>
2017-05-17 10:28:02 +0300

    Added sh to execute .sh

--- src/main/resources/bin/epp.sh
+++ src/main/resources/bin/epp.sh
@@ -6,7 +6,7 @@ pwd
 for i in $@
 do
 	echo projecting out $i from the $PROTOCOL protocol in $SCRIBBLE into an fsm
-	$SCRIBBLEDIR/bin/scribblec.sh $SCRIBBLE  -fsm $PROTOCOL $i >$SCRIBBLEDIR/bin/generated/$i.fsm
+	sh $SCRIBBLEDIR/bin/scribblec.sh $SCRIBBLE  -fsm $PROTOCOL $i >$SCRIBBLEDIR/bin/generated/$i.fsm
 	echo transforming the fsm for $i into an easyFSM config file
-	$SCRIBBLEDIR/bin/fsmxlator.sh $SCRIBBLEDIR/bin/generated/${i}.fsm -easyFSM ${i} org.estafet.scribble.example >$SCRIBBLEDIR/bin/generated/${i}_config.txt
+	sh $SCRIBBLEDIR/bin/fsmxlator.sh $SCRIBBLEDIR/bin/generated/${i}.fsm -easyFSM ${i} org.estafet.scribble.example >$SCRIBBLEDIR/bin/generated/${i}_config.txt
 done
